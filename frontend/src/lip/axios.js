0cf984ab11f6abe97969ef20cc006c50dfef2fbd
prince-dansco <lorddansco@gmail.com>
2025-06-13 14:28:59 +0100

    adding the backend url

--- frontend/src/lip/axios.js
+++ frontend/src/lip/axios.js
@@ -1,8 +1,18 @@
+// import axios from "axios";
+
+// const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5001/api': 'https://backend-note-think-board.onrender.com/api'
+
+// const api = axios.create({
+//     baseURL: BASE_URL,
+// })
+// export default api;
+
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5001/api': '/api'
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api'
 
 const api = axios.create({
     baseURL: BASE_URL,
 })
+
 export default api;
