e94238184ad9f07ef9f7677c9b0a5af4a92222e8
prince-dansco <lorddansco@gmail.com>
2025-06-15 21:39:25 +0100

    w

--- frontend/src/lip/axios.js
+++ frontend/src/lip/axios.js
@@ -8,9 +8,18 @@
     
     import axios from "axios";
     
-    const BASE_URL = import.meta.env.MODE === 'production' ? 'https://backend-note-think-board.onrender.com/api' : 'http://localhost:5001/api'
+    // const BASE_URL = import.meta.env.MODE === 'production' ? 'https://backend-note-think-board.onrender.com/api' : 'http://localhost:5001/api'
 // const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001/api'
 
+    // const BASE_URL = import.meta.env.MODE === 'production'
+    // ? 'https://backend-note-think-board-1.onrender.com/api'
+    // : 'http://localhost:5001/api';
+
+
+    const BASE_URL = import.meta.env.VITE_NODE_ENV === 'production' 
+    ? 'https://backend-note-think-board-1.onrender.com/api' 
+    : 'http://localhost:5001/api';
+    
 const api = axios.create({
     baseURL: BASE_URL,
 })
